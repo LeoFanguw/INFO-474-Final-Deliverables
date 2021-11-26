e4439346bdef54cac332cfb27e4e8dbdbfa98624
Leo Fang <rfang2@uw.edu>
2021-11-26 13:38:22 +0800

    finish layout

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
 var svg = d3.select('svg');
 d3.csv('dataset.CSV').then(function(dataset) {
 });
