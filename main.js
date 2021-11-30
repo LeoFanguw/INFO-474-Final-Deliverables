51a10a19aab7b48fce49196cd9a1fdece5d7d947
Suzy Nie <qingnie@uw.edu>
2021-11-29 20:04:27 -0800

    base map

--- main.js
+++ main.js
@@ -9,5 +9,32 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
 })
 
 var svg = d3.select('svg');
-d3.csv('dataset.CSV').then(function(dataset) {
+
+var svgWidth = +svg.attr('width');
+var svgHeight = +svg.attr('height');
+
+// d3.csv('dataset.CSV').then(function (dataset) {});
+
+
+const projection = d3.geoMercator().scale(140)
+    .translate([svgWidth / 2, svgHeight / 1.4]);
+const path = d3.geoPath(projection);
+
+const g = svg.append('g');
+
+d3.json('https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json').then(data => {
+
+  const countries = topojson.feature(data, data.objects.countries);
+
+  const g = svg.append('g');
+
+
+  g.selectAll('path')
+    .data(countries.features)
+    .enter()
+    .append('path')
+    .attr('fill', 'green')
+    .attr('stroke', 'white')
+    .attr('class', 'country')
+    .attr('d', path);
 });
